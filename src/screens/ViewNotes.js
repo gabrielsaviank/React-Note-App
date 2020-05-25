c1b198743467efaba41565ea1b2087f3ac687764
Gabriel <gabrielsvnzrd@gmail.com>
2020-05-25 16:14:37 -0300

    button add note added

--- src/screens/ViewNotes.js
+++ src/screens/ViewNotes.js
@@ -1,13 +1,19 @@
 import React from  'react'
 import {StyleSheet, View} from 'react-native'
-import {Text} from 'react-native-paper'
+import {Text, FAB} from 'react-native-paper'
 
-function ViewNotes() {
+function ViewNotes({navigation}) {
     return(
         <View style= {styles.container} >
             <View style = {styles.titleContainer}>
                 <Text style= {styles.title}>Voce nao tem notas</Text>
             </View>
+            <FAB styles = {styles.fab}
+                small
+                icon = 'plus'
+                label = 'Nova Nota'
+                onPress = {() =>  navigation.navigate('AddNotes')}
+            />
         </View>
     )
 } 
@@ -26,7 +32,14 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 20
-    }
+    },
+    fab: {
+        backgroundColor: '#000000',
+        position: 'absolute',
+        margin: 20,
+        right: 0,
+        bottom: 10
+    },
 }) 
 
 export default ViewNotes
