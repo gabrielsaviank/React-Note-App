1d506291f3dc5921f1311cdca5dde9e30a19bad8
Gabriel <gabrielsvnzrd@gmail.com>
2020-05-25 10:20:24 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Native Note App 
+
+This is a basic React Native Note App 
+
+_Npm Version 6.14.4_
+
+_Node Version 14.2.0_
---
+++ src/ViewNotes.js
@@ -0,0 +1,13 @@
+import React from  'react'
+import {StyleSheet, View} from 'react-native'
+import {Text} from 'react-native-paper'
+
+function ViewNotes() {
+    return(
+        <View>
+            <View>
+                <Text>Voce nao tem notas</Text>
+            </View>
+        </View>
+    )
+}
